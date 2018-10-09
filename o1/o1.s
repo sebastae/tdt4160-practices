d17ddf54c1b4ab3033c3340f19cf6e40ed265602
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2018-10-09 09:20:05 +0200

    Fix logic error based on PB0 being active low

--- o1/o1.s
+++ o1/o1.s
@@ -51,23 +51,23 @@ Start:
 
 // Av en eller annen grunn skrur R12 Av og R11 på?
 store_on:
-	STR R9, [R11]	// Set LED-pin i LED_DOUTSET til høy (skru på LED), fortsetter så til neste instruksjon (test_on_state)
+	STR R9, [R12]	// Set LED-pin i LED_DOUTSET til høy (skru på LED), fortsetter så til neste instruksjon (test_on_state)
 
 test_on_state:
 	LDR R5, [R10]	// Les alle verdier i BUTTON_PORT
 	AND R6, R5, R8	// Velg bare verdi på button pin
 	CMP R6, #0		// Sjekk om verdi er 0 -> Knapp er ikke trykket ned
-	BEQ	store_off	// Knappen er ikke trykket ned, skru av LED og gå til av-sjekk
+	BNE	store_off	// Knappen er ikke trykket ned, skru av LED og gå til av-sjekk, active-low på knappen -> ikke nede = høyt signal
 	B test_on_state	// Knappen er trykket ned, start ny sjekk
 
 store_off:
-	STR R9, [R12]	// Set LED-pin på i LED_DOUTCLR (skru av LED)
+	STR R9, [R11]	// Set LED-pin på i LED_DOUTCLR (skru av LED)
 
 test_off_state:
 	LDR R5, [R10]	// Les BUTTON_PORT
 	AND R6,	R5, R8	// Velg button-pin
 	CMP R6, #0		// Sammenligne button-pin og 0
-	BNE store_on	// Om de ikke er like (0) er knappen trykket ned, skru på LED
+	BEQ store_on	// Om de ikke er like (0) er knappen trykket ned, skru på LED
 	B test_off_state// Fortsett å sjekke til knappen blir trykket ned
 
 
